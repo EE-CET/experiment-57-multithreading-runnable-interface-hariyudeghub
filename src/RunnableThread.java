58c60b08dba00fbfd68958a6edbe21704f64b051
hariyudeghub <harikrishnas9a@gmail.com>
2026-02-21 10:56:22 +0530

    thread

--- src/RunnableThread.java
+++ src/RunnableThread.java
@@ -1,16 +1,16 @@
 class MyRunnable implements Runnable {
     @Override
-    public void run(){
-        for(int i=1;i<=5;i++){
-            System.out.println(i+"");
+    public void run() {
+        for (int i = 1; i <= 5; i++) {
+            System.out.print(i + " ");
         }
     }
 }
-
 public class RunnableThread {
     public static void main(String[] args) {
-        MyRunnable obj=new MyRunnable();
-        Thread t=new Thread(obj);
+
+        MyRunnable obj = new MyRunnable();
+        Thread t = new Thread(obj);
         t.start();
     }
 }
